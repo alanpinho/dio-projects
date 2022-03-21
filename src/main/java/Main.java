f88c9e49e73f7d855302a00de2d92d4296258d83
Alan Pinho <alanpinho.scm@gmail.com>
2022-03-21 12:32:59 -0300

    As classes Bootcamp, Conteudo e Dev foram adicionadas. Também implementamos alguns métodos nas classes de domínio.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -13,15 +15,45 @@ public class Main {
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso de Golang");
         curso2.setDescricao("Descrição do Curso de Golang");
-        curso2.setCargaHoraria(8);
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria de Java");
         mentoria.setDescricao("Descrição da mentoria de Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição do Bootcamp Java Developer");
+        bootcamp.getConteudosDoBootcamp().add(curso1);
+        bootcamp.getConteudosDoBootcamp().add(curso2);
+        bootcamp.getConteudosDoBootcamp().add(mentoria);
+
+        Dev devAlan = new Dev();
+        devAlan.setNome("Alan");
+        devAlan.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos Inscritos Alan: " + devAlan.getConteudosInscritos());
+        System.out.println("-");
+        devAlan.progredir();
+        devAlan.progredir();
+        System.out.println("Conteúdos Inscritos Alan: " + devAlan.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Alan: " + devAlan.getConteudosConcluidos());
+        System.out.println("XP: " + devAlan.calcularTotalXp());
+
+        System.out.println("-----------------------------------------------------");
+
+        Dev devMaria = new Dev();
+        devMaria.setNome("Maria");
+        devMaria.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos Inscritos Maria: " + devMaria.getConteudosInscritos());
+        System.out.println("-");
+        devMaria.progredir();
+        devMaria.progredir();
+        devMaria.progredir();
+        System.out.println("Conteúdos Inscritos Maria: " + devMaria.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Maria: " + devMaria.getConteudosConcluidos());
+        System.out.println("XP: " + devMaria.calcularTotalXp());
     }
 }
